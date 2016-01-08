400a9b5c775640d8ab85f1312f0972c935471087
HendricksK <kurvinuniversity@gmail.com>
2016-01-08 15:52:24 +0200

    mobile fix for the orbit-indicator, as well as moving the js to appropriate file, tested on IE, chrome, firefox

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+$('.orbit-indicator li').on( 'click', function(e){
+	$('li.active').removeClass('active');
+	$(this).addClass('active');
+	var slide = $(this).attr('data-id');
+	$('.orbit-slide'+slide+'').addClass('is-active');
+
+		$('.orbit-next').click(); //its a little bit of a hack
+	});
+
+$('.menu .button').on( 'click', function(e){
+	$('.button.active').removeClass('active');
+	$(this).addClass('active');
+});
+
+$('#category-holder a').on( 'click', function(e){
+	$('#category-holder a').removeClass('active');
+	$(this).addClass('active');
+});
